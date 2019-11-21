385055e4a4b88684d888911d08c742aab72a1e09
DustinG98 <thedustingraham@gmail.com>
2019-11-20 23:14:54 -0700

    added styled components and fixed a bug with the add a card

--- trello-functionality/src/components/App.js
+++ trello-functionality/src/components/App.js
@@ -4,6 +4,11 @@ import { connect } from 'react-redux'
 import TrelloActionButton from './TrelloActionButton'
 import { DragDropContext } from 'react-beautiful-dnd'
 import { sort } from '../actions'
+import styled from 'styled-components'
+
+const ListContainer = styled.div`
+  display: flex;
+`
  
 function App(props) {
   const { lists } = props;
@@ -27,22 +32,17 @@ function App(props) {
     <DragDropContext onDragEnd={onDragEnd}>
     <div className="App">
       <h2>Hello World!</h2>
-      <div style={styles.listsContainer}>
-      { lists.map(list => {
-      return <TrelloList listID={list.id} key={list.id} title={list.title} cards={list.cards}/>; 
-      }) }
-      <TrelloActionButton list />
-      </div>
+      <ListContainer>
+        { lists.map(list => {
+        return <TrelloList listID={list.id} key={list.id} title={list.title} cards={list.cards}/>; 
+        }) }
+        <TrelloActionButton list />
+      </ListContainer>
     </div>
     </DragDropContext>
   );
 }
 
-const styles = {
-  listsContainer: {
-    display: "flex"
-  }
-}
 
 const mapStateToProps = state => ({
   lists: state.lists
--- trello-functionality/src/components/TrelloList.js
+++ trello-functionality/src/components/TrelloList.js
@@ -2,33 +2,36 @@ import React from 'react';
 import TrelloCard from './TrelloCard'
 import TrelloActionButton from './TrelloActionButton'
 import { Droppable } from 'react-beautiful-dnd'
+import styled from 'styled-components'
+
+const ListContainer = styled.div`
+background-color: #dfe3e6;
+border-radius: 3px;
+width: 300px;
+height: 100%;
+padding: 8px;
+margin-right: 8px;
+display: flex;
+flex-direction: column;
+`
 
 const TrelloList = (props) => {
     const { title, cards, listID } = props;
     return(
         <Droppable droppableId={String(listID)}>
             {(provided) => {
-              return <div {...provided.droppableProps} ref={provided.innerRef} style={styles.container}>
+              return <ListContainer {...provided.droppableProps} ref={provided.innerRef}>
                 <h4>{title}</h4>
                 {(cards !== undefined) ? cards.map((card, index) => <TrelloCard index={index} id={card.id} key={card.id} text={card.text}/>) : null}
-                    <TrelloActionButton listID={listID}/>
+                    
                 {provided.placeholder}
-              </div>  
+                <TrelloActionButton listID={listID}/>
+              </ListContainer>  
             }}
             
         </Droppable>
     )
 }
 
-const styles = {
-    container: {
-        backgroundColor: "#dfe3e6",
-        borderRadius: 3,
-        width: 300,
-        height: "100%",
-        padding: 8,
-        marginRight: 8
-    }
-}
 
 export default TrelloList;
